728a530398723ab9dbf2e7201b10a91e4cb99326
mkki <gosorn1@gmail.com>
2021-03-07 20:01:10 +0900

    Add test files

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import { Test } from "./test";
+
+const test = new Test();
+
+console.log(test.name);
+
+console.log(test.testPromise());
+console.log(test.testObjectAssign());
+console.log(test.testArrayFrom());
